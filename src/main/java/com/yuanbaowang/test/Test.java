0fe3e280af45e81e72e1badb799280f291da77b9
yuanbaowang <2637562166@qq.com>
2019-12-05 08:18:42 +0800

    chu hua shi ti jiao

---
+++ src/main/java/com/yuanbaowang/test/Test.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.yuanbaowang.test;
+
+/**
+ * @author 袁保旺
+ *
+ * 2019年12月5日 上午8:12:23 
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("helloWord");
+	}
+
+}
---
+++ src/main/java/com/yuanbaowang/test/TestMain.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.yuanbaowang.test;
+
+/**
+ * @author 袁保旺
+ *
+ * 2019年12月5日 上午8:10:43 
+ * 创建主方法
+ */
+public class TestMain {
+	public static void main(String[] args) {
+		System.out.println("HelloWord");
+	}
+}
